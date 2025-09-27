b7733b3402bb4de138c9558ed4025be8d679215f
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-09-27 12:38:52 +0700

    fix: endpoint-products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,11 +12,13 @@ class ProductController extends Controller
         $request->validate([
             'name'          => ['required', 'string'],
             'category_id'   => ['required'],
+            'business_did'  => ['required']
         ]);
 
         $product = Product::create([
             'name'          => $request->name,
             'category_id'   => $request->category_id,
+            'business_id'   => $request->business_id
         ]);
 
         return response([
