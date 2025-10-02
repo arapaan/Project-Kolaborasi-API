465e1082e4d8a00a4f58cc88ed66bc2f1c0f289f
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-02 22:52:56 +0700

    Squashed commit of the following:
    commit 2a902549d6d758234dfb305373cf4b09db47f259
    Author: Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
    Date:   Thu Oct 2 14:03:34 2025 +0700
    
        add: Category CRUD

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Models\category as ModelsCategory;
+use App\Filament\Resources\CategoryResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Business;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = ModelsCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+                Select::make('business_id')
+                    ->label('Business')
+                    ->options(Business::all()
+                        ->pluck('name_company', 'id')
+                        ->map(fn ($label) => $label ?? '-')
+                    )
+                    ->required(),
+                Select::make('parent_id')
+                    ->label('Parent')
+                    ->options(Category::all()
+                        ->pluck('name', 'id')
+                        ->map(fn ($label) => $label ?? '-')
+                    )
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('parent_id')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->sortable(),                    
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
