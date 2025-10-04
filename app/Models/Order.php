4b371dd7694399e14162cb720520a943a212c4b3
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-04 16:23:10 +0700

    fix:orders table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'business_id',
+        'user_id',
+        'created_by',
+        'status',
+        'total_price',
+        'product'
+    ];
 }
