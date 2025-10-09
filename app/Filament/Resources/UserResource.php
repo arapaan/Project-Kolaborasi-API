258509fe2c85decba40bda04fec5be24626d0bab
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-09 13:38:24 +0700

    add: users resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+use Filament\Forms\Components\Select;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->revealable()
+                    ->required(),
+                Select::make('role_id')
+                    ->relationship('role', 'name')
+                    ->required(),
+                Select::make('discount_id')
+                    ->relationship('discount', 'discount')
+                    ->required(),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()->sortable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable()->sortable(),
+                TextColumn::make('discount_id')
+                    ->label('Discount')
+                    ->searchable()->sortable(),
+                TextColumn::make('role.name')
+                    ->label('Role')
+                    ->searchable()->sortable()
+                    ->default(''),
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->searchable()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
