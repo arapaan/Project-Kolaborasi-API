24c59c7324043c39b26509434c1d46746409c161
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-17 18:49:15 +0700

    Squashed commit of the following:
    commit 5e0db92c5ed50be46c4e564d6224150df2e62a17
    Author: Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
    Date:   Fri Oct 17 17:23:25 2025 +0700
    
        add: page table

---
+++ database/migrations/2025_10_17_101218_create_page_section_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('page_section', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('page_id');
+            $table->string('section_name');
+            $table->string('section_key')->nullable();
+            $table->json('content')->nullable();
+            $table->integer('order');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('page_section');
+    }
+};
