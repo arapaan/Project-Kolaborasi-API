10ef589f74db3ce13ee64ff8b153a35f921d0566
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-18 15:19:17 +0700

    add: Page template Resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Business;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,43 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $business = Business::firstOrCreate([
+            'name_company'  => 'Sultan Java',
+            'email'         => 'SultanJava@gmail.com',
+            'phone'         => 6283838402905,
+            'address'        => 'Pasar Kembangsari, Kec. Tengaran, Kabupaten Semarang, Jawa Tengah',
+        ]); 
+        $adminRole = Role::firstOrCreate(['name' => 'admin', 'business_id' => 1]);
+        $employeeRole = Role::firstOrCreate(['name' => 'employee', 'business_id' => 1]);
+        $customerRole = Role::firstOrCreate(['name' => 'customer', 'business_id' => 1]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+         $admin = User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Arfan',
+                'password' => bcrypt('13456789'),
+            ]
+        );
+
+        $employee = User::firstOrCreate(
+            ['email' => 'employee@gmail.com'],
+            [
+                'name' => 'Budi',
+                'password' => bcrypt('13456789'),
+            ]
+        );
+
+        $customer = User::firstOrCreate(
+            ['email' => 'customer@gmail.com'],
+            [
+                'name' => 'Aly',
+                'password' => bcrypt('13456789'),
+            ]
+        );
+
+        // Assign role ke user
+        $admin->assignRole($adminRole);
+        $employee->assignRole($employeeRole);
+        $customer->assignRole($customerRole);
     }
 }
