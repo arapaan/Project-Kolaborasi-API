7abcaf8459a4f3267a5903c136f2c4602a161862
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-10-25 23:17:04 +0700

    add: column social media on business table

---
+++ database/migrations/2025_10_25_161144_add_sm_column_on_business_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->string('link_instagram')->nullable();
+            $table->string('link_facebook')->nullable();
+            $table->string('link_whatsapp')->nullable();
+            $table->string('link_twitter')->nullable();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->dropColumn('link_instagram');
+            $table->dropColumn('link_facebook');
+            $table->dropColumn('link_whatsapp');
+            $table->dropColumn('link_twitter');
+        });
+    }
+};
