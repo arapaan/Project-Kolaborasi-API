2b80bf99b79e6882118476dcee721c2b304baab8
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-11-20 10:33:28 +0700

    fix: error

--- config/midtrans.php
+++ config/midtrans.php
@@ -5,4 +5,4 @@ return [
     'server_key'    => env('MIDTRANS_SERVER_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_santized'   => env('MIDTRANS_IS_SANITIZED', true),
-]
+];
